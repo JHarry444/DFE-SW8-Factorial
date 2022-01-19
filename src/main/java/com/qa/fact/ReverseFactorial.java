11497b6e8414173fe03e9476b7572846b3f0daed
jordan <jharrison1@qa.com>
2022-01-19 10:20:20 +0000

    added solution

--- src/main/java/com/qa/fact/ReverseFactorial.java
+++ src/main/java/com/qa/fact/ReverseFactorial.java
@@ -2,4 +2,15 @@ package com.qa.fact;
 
 public class ReverseFactorial {
 
+	public String reverse(double target) {
+		for (int i = 1; target >= 1; i++) {
+			target /= i;
+			if (target == 1) {
+				return i + "!";
+			}
+		}
+
+		return "NONE";
+	}
+
 }
--- src/test/java/com/qa/fact/ReverseFactorialTest.java
+++ src/test/java/com/qa/fact/ReverseFactorialTest.java
@@ -1,5 +1,40 @@
 package com.qa.fact;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
 public class ReverseFactorialTest {
 
+	private ReverseFactorial fact = new ReverseFactorial();
+
+	@Test
+	public void test1Factorial() {
+		assertEquals("1!", this.fact.reverse(1));
+	}
+
+	@Test
+	public void test2Factorial() {
+		assertEquals("2!", this.fact.reverse(2));
+	}
+
+	@Test
+	public void test3Factorial() {
+		assertEquals("3!", this.fact.reverse(6));
+	}
+
+	@Test
+	public void test4Factorial() {
+		assertEquals("4!", this.fact.reverse(24));
+	}
+
+	@Test
+	public void test5Factorial() {
+		assertEquals("5!", this.fact.reverse(120));
+	}
+
+	@Test
+	public void testNoneFactorial() {
+		assertEquals("NONE", this.fact.reverse(99));
+	}
 }
